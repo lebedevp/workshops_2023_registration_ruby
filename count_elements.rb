f7a999a0ad90d68659b3394633ce131cd4016810
lebedevp <lebedevp9@gmail.com>
2023-04-06 12:56:11 +0200

    Count elements method completed

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,11 @@
 class CountElements
+  def initialize(array)
+    @array = array
+  end
 
+  def perform
+    hash = {}
+    @array.each { |element| hash[element] = @array.count(element) }
+    hash
+  end
 end
