5b8d10c619580c9b1d250a55f040db632603b6e9
lebedevp <lebedevp9@gmail.com>
2023-04-06 13:00:45 +0200

    Validate Password Method Completed

--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,18 @@
 class ValidatePassword
+  def initialize(password)
+    @password = password
+  end
 
+  def perform
+    return false unless @password.length.between?(6, 24)
+
+    return false unless @password =~ /[A-Z]/
+    return false unless @password =~ /[a-z]/
+    return false unless @password =~ /\d/
+    return false unless @password =~ /[!@#$%&*+=:;?<>\[\]{}()|\/^~`.-]/
+
+    return false if @password =~ /(.)\1{2,}/
+
+    true
+  end
 end
